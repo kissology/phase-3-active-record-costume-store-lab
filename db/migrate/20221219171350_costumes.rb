bf4115ff86d2c76e49b78d08b8aee16a5fabed0b
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-19 12:38:15 -0500

    commit tree 73e0224ed7ce379f58ea63c1106e4e9555d30999

---
+++ db/migrate/20221219171350_costumes.rb
@@ -0,0 +1,11 @@
+class Costume < ActiveRecord::Migration[6.1]
+  def change
+      create_table :costumes do |t|
+          t.string :name
+          t.float :price
+          t.strong :size
+          t.string :image_url
+          t.timestamps 
+      end
+    end
+end
